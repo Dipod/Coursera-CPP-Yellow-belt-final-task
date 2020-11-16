8f2ecef9b198d77075e1878241fac9f739cc7f17
Dipod <dipod.dopid@gmail.com>
2020-11-16 11:49:27 +0300

    Implemented Date class and logical operators for this class

--- src/date.cpp
+++ src/date.cpp
@@ -1,1 +1,68 @@
+#include "date.h"
 
+std::ostream& operator<<(std::ostream &stream, const Date &date) {
+	stream << std::setfill('0') << std::setw(4) << date.year << '-' << std::setw(2)
+			<< date.month << '-' << std::setw(2) << date.day;
+	return stream;
+}
+
+bool operator<(const Date &lhs, const Date &rhs) {
+	auto lhs_key = std::make_tuple(lhs.year, lhs.month, lhs.day);
+	auto rhs_key = std::make_tuple(rhs.year, rhs.month, rhs.day);
+	return lhs_key < rhs_key;
+}
+
+bool operator<=(const Date &lhs, const Date &rhs){
+	auto lhs_key = std::make_tuple(lhs.year, lhs.month, lhs.day);
+	auto rhs_key = std::make_tuple(rhs.year, rhs.month, rhs.day);
+	return lhs_key <= rhs_key;
+}
+
+bool operator>(const Date &lhs, const Date &rhs){
+	auto lhs_key = std::make_tuple(lhs.year, lhs.month, lhs.day);
+	auto rhs_key = std::make_tuple(rhs.year, rhs.month, rhs.day);
+	return lhs_key > rhs_key;
+}
+
+bool operator>=(const Date &lhs, const Date &rhs){
+	auto lhs_key = std::make_tuple(lhs.year, lhs.month, lhs.day);
+	auto rhs_key = std::make_tuple(rhs.year, rhs.month, rhs.day);
+	return lhs_key >= rhs_key;
+}
+
+bool operator==(const Date &lhs, const Date &rhs){
+	auto lhs_key = std::make_tuple(lhs.year, lhs.month, lhs.day);
+	auto rhs_key = std::make_tuple(rhs.year, rhs.month, rhs.day);
+	return lhs_key == rhs_key;
+}
+
+bool operator!=(const Date &lhs, const Date &rhs){
+	auto lhs_key = std::make_tuple(lhs.year, lhs.month, lhs.day);
+	auto rhs_key = std::make_tuple(rhs.year, rhs.month, rhs.day);
+	return lhs_key != rhs_key;
+}
+
+Date ParseDate(std::istringstream &ss_date) {
+
+	int year, month, day;
+	char separator1, separator2;
+
+	ss_date >> year >> separator1 >> month >> separator2 >> day;
+
+	if (ss_date.fail() || separator1 != '-' || separator2 != '-'
+			|| ss_date.rdbuf()->in_avail() > 0) {
+		throw std::invalid_argument("Wrong date format");
+	} else {
+		if (month < 1 || month > 12) {
+			throw std::invalid_argument(
+					"Month value is invalid: " + std::to_string(month));
+		}
+
+		if (day < 1 || day > 31) {
+			throw std::invalid_argument(
+					"Day value is invalid: " + std::to_string(day));
+		}
+	}
+
+	return Date(year, month, day);
+}
--- src/date.h
+++ src/date.h
@@ -1,1 +1,32 @@
 #pragma once
+
+#include <sstream>
+#include <iomanip>
+#include <tuple>
+
+class Date {
+public:
+	Date(const int &Year, const int &Month, const int &Day) :
+			year(Year), month(Month), day(Day) {
+	}
+
+	const int year;
+	const int month;
+	const int day;
+};
+
+std::ostream& operator<<(std::ostream &stream, const Date &date);
+
+bool operator<(const Date &lhs, const Date &rhs);
+
+bool operator<=(const Date &lhs, const Date &rhs);
+
+bool operator>(const Date &lhs, const Date &rhs);
+
+bool operator>=(const Date &lhs, const Date &rhs);
+
+bool operator==(const Date &lhs, const Date &rhs);
+
+bool operator!=(const Date &lhs, const Date &rhs);
+
+Date ParseDate(std::istringstream &ss_date);
