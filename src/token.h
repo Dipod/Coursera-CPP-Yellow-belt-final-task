02211cfbe6c800e211b96aff885a68949bde2613
Dipod <dipod.dopid@gmail.com>
2020-11-16 12:20:37 +0300

    Updated tokenizer

--- src/token.h
+++ src/token.h
@@ -2,15 +2,14 @@
 
 #include <sstream>
 #include <vector>
-using namespace std;
 
 enum class TokenType {
 	DATE, EVENT, COLUMN, LOGICAL_OP, COMPARE_OP, PAREN_LEFT, PAREN_RIGHT,
 };
 
 struct Token {
-	const string value;
+	const std::string value;
 	const TokenType type;
 };
 
-vector<Token> Tokenize(istream &cl);
+std::vector<Token> Tokenize(std::istream &cl);
